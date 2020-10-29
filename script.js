592a463b4ab82a5b0a71671ddbc99e3f43950b38
Araz Faraji <54967124+ArazFaraji@users.noreply.github.com>
2020-10-29 18:19:29 -0400

    fix

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const fetch = require('node-fetch');
+import fetch from 'node-fetch';
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
